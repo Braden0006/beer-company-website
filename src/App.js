ad9cab46f2e702baab7ffefa8e10791de7660d86
Braden0006 <bradenwhitch@yahoo.com>
2022-06-06 16:03:51 -0600

    Changed the UI and color of the "Business" and "Homepage" component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Homepage from "./Components/Homepage/Homepage";
 import Business from "./Components/Business/Business";
-import Hours from "./Components/Hours/Hours";
 import Contact from "./Components/Contact/Contact";
 
 import { Route, Routes } from "react-router-dom";
@@ -27,7 +26,6 @@ function App() {
               <Navbar open={open} isOpen={isOpen} />
               <Homepage />
               <Business />
-              <Hours />
             </div>
           </div>
         }
--- src/Components/Business/Business.js
+++ src/Components/Business/Business.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import './Business.css'
+import React from "react";
+import "./Business.css";
 
 export default function Business() {
-
   return (
-    <div className='business'>
-        <div className="business__image"></div>
-        <br />
-        <div className='business__address-image'></div>
+    <div className="business">
+      <div className="business__image"></div>
+      <div className="business__info">
+        <h1 className="business__info__title">Welcome to Oil City</h1>
+        <p className="business__info__description">Based in Casper, Wyoming, and ready to bring the best tasting beer that's been brought here</p>
+      </div>
     </div>
-  )
+  );
 }
